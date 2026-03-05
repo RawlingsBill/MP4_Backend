6af702f849042e237cb1fa57cdb6020674e6efd5
Bill <98163191+RawlingsBill@users.noreply.github.com>
2026-03-05 15:01:02 -0600

    Remove NotImplementedError from database functions
    Removed NotImplementedError raises for database functions.

--- application.py
+++ application.py
@@ -135,7 +135,6 @@ def insert_data_into_db(payload):
         connection.commit()
     finally:
         connection.close()
-    raise NotImplementedError("Database insert function not implemented.")
 
 #Database Function Stub
 def fetch_data_from_db():
@@ -169,7 +168,6 @@ def fetch_data_from_db():
         return result
     finally:
         connection.close()
-    raise NotImplementedError("Database fetch function not implemented.")
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
